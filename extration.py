fd0339c33725fafc39672441021e62018d68b845
ChrisZZhong <90562417+ChrisZZhong@users.noreply.github.com>
2022-03-13 10:58:29 -0400

    extract datasets

---
+++ extration.py
@@ -0,0 +1,30 @@
+import numpy
+import pandas as pd
+import datetime
+
+
+def extractUsCovid():
+    df = pd.read_csv("./Raw/United_States_COVID-19_Cases_and_Deaths_by_State_over_Time.csv", encoding="utf-8")
+    keep = ['submission_date', 'state', 'tot_cases', 'new_case', 'tot_death', 'new_death']
+    df = df[keep]
+    # format datetime
+    df = df.rename(columns={"submission_date": "date"}, errors="raise")
+    df['date'] = pd.to_datetime(df['date'])
+    df = df.sort_values(by="date")
+    df = df.reset_index(drop=True)
+    df.to_csv("./Extraction data/US_Covid_stateLevel.csv", index=False)
+
+
+def extractVaccination():
+    df = pd.read_csv("./Raw/us_state_vaccinations.csv", encoding="utf-8")
+    print(df.columns)
+    keep = ['date', 'location', 'total_vaccinations', 'total_distributed',
+            'people_vaccinated', 'people_fully_vaccinated',
+            'daily_vaccinations', 'share_doses_used', 'total_boosters']
+    df['date'] = pd.to_datetime(df['date'])
+    df = df.sort_values(by="date")
+    df = df.reset_index(drop=True)
+    df.to_csv("./Extraction data/USVaccination.csv", index=False)
+
+extractUsCovid()
+extractVaccination()
